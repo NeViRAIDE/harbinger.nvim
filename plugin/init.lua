03e4e171254a5c61ac509c2ed8f44978a348364e
RAprogramm <andrey.rozanov.vl@gmail.com>
2024-05-20 15:51:36 +0900

    init

---
+++ plugin/init.lua
@@ -0,0 +1,8 @@
+vim.defer_fn(function()
+  local bufs = vim.api.nvim_list_bufs()
+
+  if #vim.fn.argv() == 0 and (#bufs == 1 and bufs[1] == 1) then
+    require('harbinger').open()
+    vim.api.nvim_exec2(':bd#', { output = true })
+  end
+end, 0)
