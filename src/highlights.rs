327b7cc0ac71ec31fe4a5b6e57b9b70df6d93aa4
RAprogramm <andrey.rozanov.vl@gmail.com>
2024-10-07 20:19:17 +0900

    fix: highlights

--- src/highlights.rs
+++ src/highlights.rs
@@ -1,44 +1,53 @@
-// use nvim_oxi::api::create_namespace;
 use nvim_oxi::api::{opts::SetHighlightOpts, set_hl};
 
 use crate::error::{handle_error, PluginError};
 
 pub fn setup_highlight_groups() -> Result<(), PluginError> {
-    nvim_oxi::print!("Set up highlightings");
-    // let ns_id = create_namespace("harbinger");
-    // Создаем группу подсветки для заголовков
-    let header_highlight = SetHighlightOpts::builder()
-        .foreground("#FF0000")
-        .bold(true)
-        .build();
-
     handle_error(
-        set_hl(0, "HarbingerHeader", &header_highlight),
+        set_hl(
+            0,
+            "HarbingerHeader",
+            &SetHighlightOpts::builder()
+                .foreground("#FF0000")
+                .bold(true)
+                .build(),
+        ),
         "Failed to set highlight for HarbingerHeader",
     )?;
 
-    nvim_oxi::api::command("highlight HarbingerHeader guifg=#FF00FF")?;
-
-    // Создаем группу подсветки для кнопок
-    let button_highlight = SetHighlightOpts::builder()
-        .foreground("#00FF00")
-        .background("#000000")
-        .italic(true)
-        .build();
+    handle_error(
+        set_hl(
+            0,
+            "HarbingerSubHeader",
+            &SetHighlightOpts::builder()
+                .link("Comment")
+                .italic(true)
+                .build(),
+        ),
+        "Failed to set highlight for HarbingerHeader",
+    )?;
 
     handle_error(
-        set_hl(0, "HarbingerButton", &button_highlight),
+        set_hl(
+            0,
+            "HarbingerButton",
+            &SetHighlightOpts::builder()
+                .foreground("#00FF00")
+                .italic(true)
+                .build(),
+        ),
         "Failed to set highlight for HarbingerButton",
     )?;
 
-    let footer_highlight = SetHighlightOpts::builder()
-        .foreground("#FF0000") // Цвет текста
-        .background("#000000") // Цвет фона
-        .bold(true) // Жирный шрифт
-        .build();
-
     handle_error(
-        set_hl(0, "HarbingerFooter", &footer_highlight),
+        set_hl(
+            0,
+            "HarbingerFooter",
+            &SetHighlightOpts::builder()
+                .link("Comment")
+                .bold(true) // Жирный шрифт
+                .build(),
+        ),
         "Failed to set highlight for HarbingerFooter",
     )?;
 
